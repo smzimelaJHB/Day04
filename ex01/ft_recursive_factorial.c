cb3e97f2f38e2d7155804283bc4a2fd872cb38ca
Siyabonga Mzimela <smzimela@c4r12s1.wethinkcode.co.za>
2020-06-25 14:57:14 +0200

    final update

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -3,16 +3,21 @@
 
 int ft_recursive_factorial(int nb)
 { 
-
-	if(nb<=1)
-	{
-		return 1;
-	}
-	else
+	
+	if(nb>1)
 	{	 
      int t = nb*ft_recursive_factorial(nb-1);
      return t;
 	}
+	if(nb==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+
 }
 
 
